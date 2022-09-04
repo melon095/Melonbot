43c7a09779d4dcd2799f8c4a8fb242724a34a50c
Joachim Flottorp <joachim@flottorp.org>
2022-09-04 22:17:31 +0200

    Frontend can remove, edit and add banphrases

--- src/web/routes/api/v1/channel/banphrase/index.ts
+++ src/web/routes/api/v1/channel/banphrase/index.ts
@@ -1,3 +1,10 @@
+interface PutRequestBody {
+	id: number;
+	type: 'pb1' | 'regex';
+	regex: string;
+	pb1: string;
+}
+
 export default (async function () {
 	const Express = await import('express');
 	const Router = Express.Router();
@@ -7,12 +14,7 @@ export default (async function () {
 		const { id } = req.params as { id: string };
 
 		const cookie = req.cookies['token'];
-		if (!cookie) {
-			res.status(401).json({ error: 'Unauthorized' });
-			return;
-		}
-
-		const jwt = await Authenticator.DecryptJWT(cookie);
+		const jwt = Authenticator.DecryptJWT(cookie);
 
 		if (!jwt) {
 			res.status(401).json({ error: 'Unauthorized' });
@@ -43,6 +45,47 @@ export default (async function () {
 		const status = await user.RemoveBanphrase(banphrase[0].id);
 
 		res.status(200).json({ id, status });
+	});
+
+	Router.put('/:id', async (req, res) => {
+		const { id } = req.params as { id: string };
+		const body = req.body as PutRequestBody;
+
+		const cookie = req.cookies['token'];
+		const jwt = Authenticator.DecryptJWT(cookie);
+
+		if (!jwt) {
+			res.status(401).json({ error: 'Unauthorized' });
+			return;
+		}
+
+		const user = await Bot.User.Get(jwt.id, jwt.name, { throwOnNotFound: false });
+		if (!user) {
+			res.status(401).json({ error: 'Unauthorized' });
+			return;
+		}
+
+		res.status(200).json({ id, status: 'ACK', body });
+	});
+
+	Router.post('/', async (req, res) => {
+		const body = req.body as PutRequestBody;
+
+		const cookie = req.cookies['token'];
+		const jwt = Authenticator.DecryptJWT(cookie);
+
+		if (!jwt) {
+			res.status(401).json({ error: 'Unauthorized' });
+			return;
+		}
+
+		const user = await Bot.User.Get(jwt.id, jwt.name, { throwOnNotFound: false });
+		if (!user) {
+			res.status(401).json({ error: 'Unauthorized' });
+			return;
+		}
+
+		res.status(200).json({ status: 'ACK', body });
 	});
 
 	return Router;
--- web/public/js/settings.js
+++ web/public/js/settings.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
+
 /**
- * @param {HTMLParagraphElement} element
+ * @param { HTMLParagraphElement } element
  */
 async function onDeleteClick(element) {
 	const row = element.closest('tr');
@@ -13,9 +15,7 @@ async function onDeleteClick(element) {
 
 	if (!confirm) return;
 
-	/**
-	 * @type {Response}
-	 */
+	/*** @type {Response} */
 	const response = await fetch(`/api/v1/channel/banphrase/${id}`, {
 		method: 'DELETE',
 	});
@@ -29,6 +29,130 @@ async function onDeleteClick(element) {
 	}
 
 	row.remove();
+}
+
+/**
+ * @param { number } id
+ */
+function onEditClick(id) {
+	const div = document.querySelector(`tr[data-id="${id}"] ~ tr#banphrase-edit-menu`);
+	const hidden = div.classList.contains('d-none');
+	if (hidden) {
+		div.classList.remove('d-none');
+	} else {
+		div.classList.add('d-none');
+	}
+}
+
+const banphraseEditPb1Container = (id) =>
+	`tr[data-id='${id}']#banphrase-edit-menu td #banphrase-edit-pb1-container`;
+const banphraseEditRegexContainer = (id) =>
+	`tr[data-id='${id}']#banphrase-edit-menu td #banphrase-edit-regex-container`;
+
+/**
+ * @param { HTMLSelectElement } element
+ * @param { number } id
+ */
+function newBanphraseTypeChange(element, id) {
+	const newValue = element.value;
+	let oldElement;
+	let newElement;
+
+	switch (newValue) {
+		case 'pb1': {
+			newElement = document.querySelector(banphraseEditPb1Container(id));
+			oldElement = document.querySelector(banphraseEditRegexContainer(id));
+			break;
+		}
+		case 'regex': {
+			newElement = document.querySelector(banphraseEditRegexContainer(id));
+			oldElement = document.querySelector(banphraseEditPb1Container(id));
+			break;
+		}
+		default: {
+			return;
+		}
+	}
+
+	newElement.classList.remove('d-none');
+	oldElement.classList.add('d-none');
+}
+
+/**
+ * @param { HTMLSelectElement } element
+ * @param { number } id
+ */
+function onAddBanphraseChange(element) {
+	const newValue = element.value;
+	const regex = 'div#banphrase-add form div#banphrase-add-regex-container';
+	const pb1 = 'div#banphrase-add form div#banphrase-add-pb1-container';
+
+	switch (newValue) {
+		case 'pb1': {
+			document.querySelector(pb1).classList.remove('d-none');
+			document.querySelector(regex).classList.add('d-none');
+			break;
+		}
+		case 'regex': {
+			document.querySelector(regex).classList.remove('d-none');
+			document.querySelector(pb1).classList.add('d-none');
+			break;
+		}
+		default: {
+			return;
+		}
+	}
+}
+
+/**
+ * @param { Event } event
+ * @param { number } id
+ */
+async function onEditSubmit(event, id) {
+	const data = {
+		id,
+		type: event.target.type.value,
+		regex: event.target.regex.value,
+		pb1: event.target.pb1.value,
+	};
+
+	const response = await fetch(`/api/v1/channel/banphrase/${id}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+
+	const json = await response.json();
+
+	console.log({ json });
+}
+
+/**
+ * @param { Event } event
+ * @param { number } id
+ */
+async function onAddSubmit(event) {
+	const data = {
+		type: event.target.type.value,
+		regex: event.target.regex.value,
+		pb1: event.target.pb1.value,
+	};
+
+	console.log({ data });
+
+	const response = await fetch(`/api/v1/channel/banphrase`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+
+	const json = await response.json();
+
+	console.log({ json });
 }
 
 /**
