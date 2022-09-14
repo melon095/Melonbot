ab17e1c8dea06ac0c7c7b730296633223729d94d
Joachim Flottorp <joachim@flottorp.org>
2022-09-14 18:29:45 +0200

    Comment

--- src/triggers/eventsub/Connect.ts
+++ src/triggers/eventsub/Connect.ts
@@ -4,7 +4,7 @@ import { IPubConnect } from 'Singletons/Redis/Data.Types.js';
 export default {
 	Type: () => 'connect',
 	Handle: ({ Version }: IPubConnect) => {
-		console.log('Connected to EventSub server', {
+		console.log('Connected to EventSub Broker', {
 			Version,
 		});
 	},
