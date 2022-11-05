c3d8d3cf96cab4023d59bfc985cbe2d239cc3e8e
JoachimF <joachim@flottorp.org>
2022-11-05 21:10:40 +0100

    Feat: Time command (#39)
    * Feat: Time command
    
    Similar to time command on unix
    
    * Remove unecessary promise and toFixed
    
    * Add proper context
    
    * Catch argparse

---
+++ src/commands/time.ts
@@ -0,0 +1,71 @@
+import { EPermissionLevel, ECommandFlags } from '../Typings/enums.js';
+import { CommandModel, TCommandContext, CommandResult } from '../Models/Command.js';
+import commands from 'web/routes/bot/commands.js';
+
+export default class extends CommandModel {
+	Name = 'time';
+	Ping = false;
+	Description = 'Time a command, similar to the time command in unix';
+	Permission = EPermissionLevel.ADMIN;
+	OnlyOffline = false;
+	Aliases = [];
+	Cooldown = 5;
+	Params = [];
+	Flags = [ECommandFlags.ALLOW_INVALID_ARGS];
+	Code = async (ctx: TCommandContext): Promise<CommandResult> => {
+		const name = ctx.input[0];
+		if (!name) {
+			return {
+				Result: 'Give a command..',
+				Success: false,
+			};
+		}
+
+		const command = await Bot.Commands.get(name);
+		if (!command) {
+			return {
+				Result: 'Command not found',
+				Success: false,
+			};
+		}
+
+		let params;
+		try {
+			params = CommandModel.ParseArguments(ctx.input.slice(1), command.Params, {
+				allowInvalid: false,
+			});
+		} catch (error) {
+			return {
+				Result: (error as Error).message,
+				Success: false,
+			};
+		}
+
+		const context: TCommandContext = {
+			...ctx,
+			input: params.input,
+			data: {
+				...ctx.data,
+				Params: params.values,
+			},
+		};
+
+		const start = performance.now();
+		const response = await command.Code(context).catch((err) => {
+			return {
+				Result: err,
+				Success: false,
+			};
+		});
+
+		const end = performance.now();
+
+		const time = (end - start).toFixed(2);
+		const resp = `${time}ms - ${response.Result}`;
+
+		return {
+			Result: resp,
+			Success: true,
+		};
+	};
+}
