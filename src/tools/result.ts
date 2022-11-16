9e12e020936bf10516c133d66f9f356d4d4e2597
Joachim Flottorp <joachim@flottorp.org>
2022-11-16 05:45:59 +0100

    Better implementation

--- src/tools/result.ts
+++ src/tools/result.ts
@@ -1,45 +1,59 @@
-type Opts<T, E> = {
-	isOk: boolean;
-	inner: T;
-	error: E;
-};
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface Base<T, E> {
+	readonly ok: boolean;
+	readonly err: boolean;
 
-export class Result<T, E> {
-	private constructor(opts: Opts<T, E>) {
-		this.isOk = opts.isOk;
-		this.inner = opts.inner;
-		this.error = opts.error;
+	/**
+	 * Return the Ok value if it exists, otherwise throw an error
+	 */
+	unwrap(): T;
+
+	/**
+	 * Return the Ok value if it exists, otherwise return the default value
+	 */
+	unwrapOr(def: () => T): T;
+}
+
+export class Ok<T> implements Base<T, never> {
+	readonly ok!: true;
+	readonly err!: false;
+	readonly inner!: T;
+
+	constructor(inner: T) {
+		this.inner = inner;
+
+		this.err = false;
+		this.ok = true;
 	}
-
-	static Ok<T>(value: T): Result<T, null> {
-		return new Result<T, null>({ isOk: true, inner: value, error: null });
-	}
-
-	static Err<E>(error: E): Result<null, E> {
-		return new Result<null, E>({ isOk: false, inner: null, error });
-	}
-
-	readonly isOk: boolean;
-	readonly inner: T;
-	readonly error: E;
 
 	unwrap(): T {
-		if (this.isOk) {
-			return this.inner;
-		}
-
-		throw this.error;
-	}
-
-	public get IsOk(): boolean {
-		return this.isOk;
-	}
-
-	public get Inner(): T {
 		return this.inner;
 	}
 
-	public get Error(): E {
-		return this.error;
+	unwrapOr<T2>(_def: () => T2): T | T2 {
+		return this.inner;
 	}
 }
+
+export class Err<E> implements Base<never, E> {
+	readonly ok!: false;
+	readonly err!: true;
+	readonly inner!: E;
+
+	constructor(inner: E) {
+		this.inner = inner;
+
+		this.err = true;
+		this.ok = false;
+	}
+
+	unwrap(): never {
+		throw this.inner;
+	}
+
+	unwrapOr<T2>(def: () => T2): T2 {
+		return def();
+	}
+}
+
+export type Result<T, E> = Ok<T> | Err<E>;
