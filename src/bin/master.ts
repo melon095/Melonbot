d35f900c4b06890e246452f306a4e259aa3a6720
Joachim Flottorp <joachim@flottorp.org>
2023-03-08 19:12:39 +0100

    fix uhh some things

--- src/bin/master.ts
+++ src/bin/master.ts
@@ -1,10 +1,9 @@
 import * as tools from './../tools/tools.js';
 import Twitch from './../Twitch.js';
-import { SevenTVEvent } from './../controller/Emote/SevenTV/EventAPI.js';
 import TimerSingleton from './../Singletons/Timers/index.js';
-import { Channel, GetSettings } from './../controller/Channel/index.js';
-import channels from 'Typings/models/bot/channels.js';
 import { ChatClient } from '@kararty/dank-twitch-irc';
+import { Channel } from '../controller/Channel/index.js';
+import ChannelTable, { ChannelDatabaseToMode } from '../controller/DB/Tables/ChannelTable.js';
 
 (async () => {
 	/* eslint-disable @typescript-eslint/ban-ts-comment */
@@ -49,9 +48,6 @@ async function initialize() {
 	// Create Twitch objects
 	Bot.Twitch = {
 		Controller: await Twitch.Init(),
-		Emotes: {
-			SevenTVEvent: new SevenTVEvent(),
-		},
 	};
 
 	await TimerSingleton.I().Initialize();
@@ -59,17 +55,16 @@ async function initialize() {
 	await Bot.Twitch.Controller.InitPromise;
 	const twitch = Bot.Twitch.Controller;
 
-	Bot.Twitch.Emotes.SevenTVEvent.Connect();
-
 	const self = await Channel.CreateBot();
 
 	await twitch.client.join(Bot.Config.BotUsername);
 	twitch.channels.push(self);
 
 	// Join all channels
-	const channelList = await Bot.SQL.Query<Database.channels[]>`
-                SELECT * FROM channels 
-                WHERE name NOT LIKE ${Bot.Config.BotUsername}`;
+	const channelList = await Bot.SQL.selectFrom('channels')
+		.selectAll()
+		.where('name', 'not like', Bot.Config.BotUsername)
+		.execute();
 
 	if (!channelList.length) {
 		return;
@@ -81,10 +76,9 @@ async function initialize() {
 	}
 }
 
-async function DoCreateChannel(client: ChatClient, channel: channels): Promise<Channel> {
-	let mode = tools.NChannelFunctions.DatabaseToMode(channel.bot_permission);
+async function DoCreateChannel(client: ChatClient, channel: ChannelTable): Promise<Channel> {
+	let mode = ChannelDatabaseToMode(channel.bot_permission);
 	const user = await Bot.User.Get(channel.user_id, channel.name);
-	let doEventsub = true;
 
 	Bot.Log.Info(`Twitch Joining %s`, channel.name);
 	try {
@@ -92,17 +86,8 @@ async function DoCreateChannel(client: ChatClient, channel: channels): Promise<C
 	} catch (error) {
 		Bot.Log.Error(error as Error, `Joining ${channel.name}`);
 		mode = 'Read'; // We want to create a channel object, but since we can't join, we set the mode to read
-		doEventsub = false;
 	}
 	const newChannel = await Channel.New(user, mode, channel.live);
-
-	if (doEventsub) {
-		const emote_set = await GetSettings(user).then(
-			(settings) => settings.SevenTVEmoteSet.ToString() ?? undefined,
-		);
-
-		await Channel.WithEventsub(newChannel, emote_set);
-	}
 
 	await tools.Sleep(Bot.Config.Verified ? 0.025 : 1);
 
@@ -111,7 +96,7 @@ async function DoCreateChannel(client: ChatClient, channel: channels): Promise<C
 
 async function exitHandler(): Promise<void> {
 	// Wait for all messages to get sent before turning off bot.
-	const promises = [Bot.Twitch.Emotes.SevenTVEvent.Close()];
+	const promises = [];
 	for (const channel of Bot.Twitch.Controller.TwitchChannels) {
 		promises.push(channel.Queue.closeAll());
 	}
