bb6ae210fdead7f399ed019782af68a1e9b4e982
JoachimFlottorp <JoachimFlottorp@users.noreply.github.com>
2023-03-23 19:47:55 +0000

    Formatted by Prettier in CI

--- src/commands/github/index.ts
+++ src/commands/github/index.ts
@@ -1,21 +1,21 @@
-import { registerCommand } from "../../controller/Commands/Handler.js";
-import { EPermissionLevel } from "./../../Typings/enums.js";
+import { registerCommand } from '../../controller/Commands/Handler.js';
+import { EPermissionLevel } from './../../Typings/enums.js';
 
 registerCommand({
-  Name: "github",
-  Ping: false,
-  Description: "Shows the git repo",
-  Permission: EPermissionLevel.VIEWER,
-  OnlyOffline: false,
-  Aliases: ["git"],
-  Cooldown: 10,
-  Params: [],
-  Flags: [],
-  PreHandlers: [],
-  Code: async function () {
-    return {
-      Success: true,
-      Result: "https://github.com/JoachimFlottorp/Melonbot",
-    };
-  },
+	Name: 'github',
+	Ping: false,
+	Description: 'Shows the git repo',
+	Permission: EPermissionLevel.VIEWER,
+	OnlyOffline: false,
+	Aliases: ['git'],
+	Cooldown: 10,
+	Params: [],
+	Flags: [],
+	PreHandlers: [],
+	Code: async function () {
+		return {
+			Success: true,
+			Result: 'https://github.com/JoachimFlottorp/Melonbot',
+		};
+	},
 });
